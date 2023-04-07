fccfb8d651aa460ed4801a81b472bb7d26112bb5
Jack Lowry <jlowry152@gmail.com>
2023-04-06 22:45:32 -0500

    First Commit

---
+++ Starter_Code/StarterCode/static/js/app.js
@@ -0,0 +1,5 @@
+// Use the D3 Library to read in samples.json from the URL:
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+
+
